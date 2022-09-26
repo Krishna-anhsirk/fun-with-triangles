d40593e44b6f5b0380c61da52db58e7b4720efb9
Krishna-anhsirk <Krishnatejajinka@gmail.com>
2022-09-26 10:31:59 +0530

    html and js done: quiz

---
+++ quiz.js
@@ -0,0 +1,20 @@
+const submitQuiz = document.querySelector(".submit-form");
+const correctAnswers = ["90°", "right-angled"];
+const quizForm = document.querySelector(".quiz-form");
+const output = document.querySelector(".output");
+
+function calculateScore() {
+  const data = new FormData(quizForm);
+  var score = 0;
+  var index = 0;
+
+  for (const val of data.values()) {
+    if (val == correctAnswers[index]) {
+      score++;
+    }
+    index++;
+  }
+  output.innerText = `Score is ${score}`;
+}
+
+submitQuiz.addEventListener("click", calculateScore);
